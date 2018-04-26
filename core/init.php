6acedc60860ea4c44f67322653d654a972122608
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2018-04-26 14:32:41 +0200

    modify spaces

--- core/init.php
+++ core/init.php
@@ -2,37 +2,35 @@
 
 session_start();
 
-
 // database configuration
 $_config = [
-    'host'		=> '127.0.0.1',
-    'username'	=> 'root',
-    'password'	=> '!23qwe',
-    'database'	=> 'MGZ'
+    'host'      => '127.0.0.1',
+    'username'  => 'root',
+    'password'  => '!23qwe',
+    'database'  => 'MGZ'
 ];
-
 
 // register classes -autoloading-
 define('DS', DIRECTORY_SEPARATOR);
 
 $coreClasses = [
-	# wall classes
-    'Post'			=> 'wall' . DS . 'Post',
-    'Comment'		=> 'wall' . DS . 'Comment',
-    'Like'			=> 'wall' . DS . 'Like',
+    # wall classes
+    'Post'      => 'wall' . DS . 'Post',
+    'Comment'   => 'wall' . DS . 'Comment',
+    'Like'      => 'wall' . DS . 'Like',
 
-	# account classes
-    'User'			=> 'account' . DS . 'User',
-    'Profile'		=> 'account' . DS . 'Profile',
-    'Friend'		=> 'account' . DS . 'Friend',
+    # account classes
+    'User'      => 'account' . DS . 'User',
+    'Profile'   => 'account' . DS . 'Profile',
+    'Friend'    => 'account' . DS . 'Friend',
 
-	# utilities classes
-    'DB'			=> 'utilities' . DS . 'DB',
-    'Validator'		=> 'utilities' . DS . 'Validator',
-    'Photo'			=> 'utilities' . DS . 'Photo',
+    # utilities classes
+    'DB'        => 'utilities' . DS . 'DB',
+    'Validator' => 'utilities' . DS . 'Validator',
+    'Photo'     => 'utilities' . DS . 'Photo',
 
-	# others
-	'Message'		=> 'Message'
+    # others
+    'Message'  => 'Message'
 ];
 
 spl_autoload_register(function($className) use ($coreClasses) {
