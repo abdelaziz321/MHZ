2192f5f45e2b5c7cff7c8a0214cf3ee383de98c6
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2018-04-29 12:55:19 +0200

    add the structure of the Profile class

--- classes/account/Profile.php
+++ classes/account/Profile.php
@@ -3,9 +3,64 @@
 
 class Profile extends User
 {
-
-	function __construct()
+    /**
+     * Calling the parent constructor
+     *
+     * @param array $data
+     */
+	function __construct($data)
 	{
-		echo "hello from Profile class";
+		super($data);
 	}
+
+    /**
+     * Update user profile using the 'info' array
+     * [firstname, lastname, nickname, email ...]
+     *
+     * @param array info
+     */
+    public static function updateProfile($info)
+    {
+
+    }
+
+    /**
+     * Get all the user data
+     *
+     * @return stdClass the result
+     */
+    public function getData()
+    {
+
+    }
+
+    /**
+     * Get the nickname if provided by the user
+     * otherwise get string of the firstname & lastname
+     *
+     * @return string
+     */
+    public function getNickname()
+    {
+
+    }
+
+    /**
+     * Upldoad the picture and Generate new name for the profile picture
+     */
+    private function profilePicture()
+    {
+
+    }
+
+    /**
+     * validating the user inforamation before updating
+     *
+     * @return bool
+     */
+    private function validateInfo()
+    {
+        
+    }
+
 }
